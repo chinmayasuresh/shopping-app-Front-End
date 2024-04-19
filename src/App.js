262b1b8781f8f8ca2ad4e8816883763f8c32653e
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-19 23:52:07 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
 import View from './components/View';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
-
+<Nav/>
 <Add/>
 <Search/>
 <View/>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+
+<div>
+<nav class="navbar navbar-expand-lg  bg-body-tertiary" data-bs-theme="danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><em><b>SHOPPING APP</b></em></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/add"><b><em>ADD PRODUCT</em></b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/search"><b><em>SEARCH PRODUCT</em></b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/view"><b><em>VIEW PRODUCT</em></b></a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/search"></a>
+        </li>
+       
+       
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-warning" href="/search" type="submit">SIGNIN</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Nav
